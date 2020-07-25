b8037c245f9699e72dbe5311d82034c974e81cd6
Sarah <lichter.sarahe@gmail.com>
2020-07-25 14:13:22 -0500

    initial commit

---
+++ PantryApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PantryApp
+//
+//  Created by Sarah Lichter on 7/25/20.
+//  Copyright © 2020 Sarah Lichter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PantryAppTests/PantryAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  PantryAppTests.swift
+//  PantryAppTests
+//
+//  Created by Sarah Lichter on 7/25/20.
+//  Copyright © 2020 Sarah Lichter. All rights reserved.
+//
+
+import XCTest
+@testable import PantryApp
+
+class PantryAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
